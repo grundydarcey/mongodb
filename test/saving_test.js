2dddeadc349d1a95e19d45bddf6bb8fb2a0c4186
Darcey Grundy <grundy.darcey@gmail.com>
2021-09-02 14:03:20 -0400

    part 6

---
+++ test/saving_test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const MarioChar = require('../models/mario');
+
+// Describe tests
+describe('Saving records', function() {
+  // Create tests
+  it('saves a record to the database', function(done) {
+    var char = new MarioChar({
+      name: 'Mario'
+    });
+    char.save().then(function() {
+      assert(char.isNew === false);
+      done();
+    });
+  });
+});
