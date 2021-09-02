b922861300d195825598739fec747e0e713a0d92
Darcey Grundy <grundy.darcey@gmail.com>
2021-09-02 15:08:17 -0400

    part 9

--- test/connection.js
+++ test/connection.js
@@ -23,3 +23,10 @@ before(function() {
 // }).on('error', function(error) {
 //   console.log('Connection error', error);
 // });
+
+// Drop the characters collection before each test
+beforeEach(function(done) {
+  mongoose.connection.collections.mariochars.drop(function() {
+    done();
+  });
+});
--- test/saving_test.js
+++ test/saving_test.js
@@ -5,9 +5,11 @@ const MarioChar = require('../models/mario');
 describe('Saving records', function() {
   // Create tests
   it('saves a record to the database', function(done) {
+ 
     var char = new MarioChar({
       name: 'Mario'
     });
+ 
     char.save().then(function() {
       assert(char.isNew === false);
       done();
