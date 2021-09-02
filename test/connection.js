26de8bb75a3aec85ce3129e67050d61e27e26035
Darcey Grundy <grundy.darcey@gmail.com>
2021-09-02 14:48:16 -0400

    part 7

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
-//connect to mongoDB
+// ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/testaroo');
-
-mongoose.connection.once('open', function() {
-  console.log('Connection has been made, now fireworks');
-}).on('error', function(error) {
-  console.log('Connection error', error);
+// connect to the db before tests run
+before(function() {
+  mongoose.connect('mongodb://localhost/testaroo');
+  mongoose.connection.once('open', function() {
+    console.log('Connection has been made, now fireworks');
+    done();
+  }).on('error', function(error) {
+    console.log('Connection error', error);
+  });
 });
+
+// // connect to mongoDB
+
+// mongoose.connect('mongodb://localhost/testaroo');
+
+// mongoose.connection.once('open', function() {
+//   console.log('Connection has been made, now fireworks');
+// }).on('error', function(error) {
+//   console.log('Connection error', error);
+// });
