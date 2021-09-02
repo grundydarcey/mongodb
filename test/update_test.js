7e12851ea11a239d1742875f96d64617264c782c
Darcey Grundy <grundy.darcey@gmail.com>
2021-09-02 16:06:02 -0400

    part 14

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,8 @@ describe('Updating records', function() {
   var char;
   beforeEach(function(done) {
     char = new MarioChar({
-      name: 'Mario'
+      name: 'Mario',
+      weight: 50
     });
   });
  
@@ -13,13 +14,20 @@ describe('Updating records', function() {
     done();
   });
 
-  it('updates a record on the database', function(done) {
+  it('updates one record on the database', function(done) {
     MarioChar.findOneAndUpdate({name: 'Mario'}, {name: 'Luigi'}).then(function() {
       MarioChar.findOne({_id: char._id}).then(function(result) {
         assert(result.name === 'Luigi');
         done();
       });
     });
-
+  });
+  it('Increments weight by 1', function(done) {
+    MarioChar.update({}, { $inc: { weight: 1 } }).then(function() {
+      MarioChar.findOne({name: 'Mario'}).then(function(record) {
+        assert(record.weight === 51);
+        done();
+      });
+    });
   });
 });
